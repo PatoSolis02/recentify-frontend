b4ce65fcab06aa7414cfba2ebf0f70ed3551bd91
pgs5983 <pgs5983@rit.edu>
2025-07-03 23:43:20 -0400

    local backend

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,12 @@
 import { BrowserRouter as Router, Routes, Route, useNavigate, useLocation } from "react-router-dom";
 import React from "react";
 
+
 function Home() {
   const handleLogin = () => {
-    window.location.href = "http://recentify.us-east-1.elasticbeanstalk.com/api/spotify/login";
+    // Use the local backend URL for login
+    window.location.href = "http://127.0.0.1:8888/api/spotify/login";
+    // window.location.href = "http://recentify.us-east-1.elasticbeanstalk.com/api/spotify/login";
   };
 
   return (
