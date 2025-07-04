1ecf57ce4c38c91c3cbb120d41d724b382c43fed
pgs5983 <pgs5983@rit.edu>
2025-07-03 23:45:26 -0400

    nvm

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,8 +5,8 @@ import React from "react";
 function Home() {
   const handleLogin = () => {
     // Use the local backend URL for login
-    window.location.href = "http://127.0.0.1:8888/api/spotify/login";
-    // window.location.href = "http://recentify.us-east-1.elasticbeanstalk.com/api/spotify/login";
+    // window.location.href = "http://127.0.0.1:8888/api/spotify/login";
+    window.location.href = "http://recentify.us-east-1.elasticbeanstalk.com/api/spotify/login";
   };
 
   return (
